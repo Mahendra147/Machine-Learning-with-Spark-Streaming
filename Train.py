cd5f680cc3d50dc83aeb0fc96cedb1c38b35b99d
Maneesh <65223389+maneeshsriram@users.noreply.github.com>
2021-12-06 23:26:23 +0530

    Add train.py

---
+++ Train.py
@@ -0,0 +1,155 @@
+import pyspark
+import numpy as np
+from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.streaming import StreamingContext
+import pyspark.sql.types as tp
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.naive_bayes import BernoulliNB
+clf = BernoulliNB()
+import joblib
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from pyspark.sql import Row,SQLContext,SparkSession
+
+from pyspark.sql import Row
+from sparknlp.base import *
+from sparknlp.annotator import *
+#from sparknlp import DocumentAssembler
+from sklearn import linear_model
+
+from pyspark.ml.linalg import Vector
+from sklearn.naive_bayes import GaussianNB
+clf69 = GaussianNB()
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from pyspark.ml.feature import *
+#from sklearn.feature_extraction.text import HashingVectorizer
+import numpy as np
+
+if __name__ == "__main__":
+	sc= SparkContext(master="local[2]",appName="trial")
+	ssc = StreamingContext(sc,5)
+	spark = SparkSession(sc)
+	lines= ssc.socketTextStream("localhost", 6100)
+	sql=SQLContext(sc)
+	
+
+	word=lines.flatMap(lambda line: line.split("\n"))
+	def main(rd):
+		arg0=[]
+		arg1=[]
+		arg2=[]
+		df= spark.read.json(rd)
+		f=df.collect()
+		for i in f:
+			for k in i:
+				arg0.append(str(k[0]))
+				arg1.append(str(k[1]))
+				arg2.append(str(k[2]))
+		if(len(arg0)!=0 and len(arg1)!=0 and len(arg2)!=0):
+			x=sql.createDataFrame(zip(arg0,arg1,arg2),schema=['Subject','Email_content','label'])
+			#x.show()
+			
+			
+			rt=RegexTokenizer(inputCol="Subject",outputCol="trial",pattern="\\W+")
+			
+			stopremove = StopWordsRemover(inputCol='trial',outputCol='stop_tokens')
+			
+			cv = CountVectorizer(inputCol="stop_tokens", outputCol="token_features", minDF=2.0)
+			
+			rt1=RegexTokenizer(inputCol="Email_content",outputCol="trial1",pattern="\\W+")
+			
+			stopremove1 = StopWordsRemover(inputCol='trial1',outputCol='stop_tokens1')
+			
+			
+			bigchunks = NGram().setN(2).setInputCol('stop_tokens').setOutputCol('bigchunks')
+			
+			bigchunks1 = NGram().setN(2).setInputCol('stop_tokens1').setOutputCol('bigchunks1')
+    			
+			ht = HashingTF(inputCol="bigchunks", outputCol="hastb",numFeatures=8000)
+			
+			ht1 = HashingTF(inputCol="bigchunks1", outputCol="hastb1",numFeatures=8000)
+			
+			label_index = StringIndexer(inputCol="label",outputCol="label_index")
+			
+			
+			nlpPipeline = Pipeline(stages=[
+	    			#rt,
+	    			rt1,
+	    			#stopremove,
+	    			stopremove1,
+	    			#cv,
+	    			#cv1,
+	    			label_index,
+	    			#bigchunks,
+	    			bigchunks1,
+	    			#ht,
+	    			ht1
+	    			])
+	    		
+			pipelineModel = nlpPipeline.fit(x)
+
+			result = pipelineModel.transform(x)
+			X = np.array(result.select("hastb1").collect())
+			Y = np.array(result.select("label_index").collect())
+			
+			nsamples, nx, ny = X.shape
+			X = X.reshape((nsamples,nx*ny))
+			
+			def bernoullinb(X,Y):
+				try:
+					buildmodel = joblib.load('/home/pes1ug19cs285/bernoullimodel.pkl')
+					buildmodel.partial_fit(X, Y.ravel())
+					joblib.dump(buildmodel, '/home/pes1ug19cs285/bernoullimodel.pkl')
+				except Exception as e:
+					newmodel=BernoulliNB()
+					newmodel.partial_fit(X, Y.ravel(),classes=np.unique(Y))
+					joblib.dump(newmodel, '/home/pes1ug19cs285/bernoullimodel.pkl')
+			def sgd(X,Y):
+				
+				try:
+					buildmodel = joblib.load('/home/pes1ug19cs285/linearmodel_SGD.pkl')
+					buildmodel.partial_fit(X, Y.ravel())
+					joblib.dump(buildmodel, '/home/pes1ug19cs285/linearmodel_SGD.pkl')
+				except Exception as e:
+					newmodel=linear_model.SGDClassifier()
+					newmodel.partial_fit(X, Y.ravel(),classes=np.unique(Y))
+					joblib.dump(newmodel, '/home/pes1ug19cs285/linearmodel_SGD.pkl')
+			def perceptron(X,Y):
+					
+				try:
+					buildmodel = joblib.load('/home/pes1ug19cs285/perceptron.pkl')
+					buildmodel.partial_fit(X, Y.ravel())
+					joblib.dump(buildmodel, '/home/pes1ug19cs285/perceptron.pkl')
+				except Exception as e:
+					newmodel=Perceptron()
+					newmodel.partial_fit(X, Y.ravel(),classes=np.unique(Y))
+					joblib.dump(newmodel, '/home/pes1ug19cs285/perceptron.pkl')
+			def clustering(X,Y):
+				try:
+					
+					buildmodel = joblib.load('/home/pes1ug19cs285/clustering.pkl')
+					buildmodel.partial_fit(X,Y.ravel())
+					joblib.dump(buildmodel, '/home/pes1ug19cs285/clustering.pkl')
+				except Exception as e:
+					#print("BLAH")
+					kmeans = MiniBatchKMeans(n_clusters=2, random_state=0)
+					kmeans.partial_fit(X,Y.ravel())
+					joblib.dump(kmeans, '/home/pes1ug19cs285/clustering.pkl')
+			global count		
+			bernoullinb(X,Y)
+			sgd(X,Y)
+			perceptron(X,Y)
+			clustering(X,Y)
+					
+			
+
+	
+	rdd=word.foreachRDD(main)
+	print("new batch")
+	ssc.start()
+	ssc.awaitTermination()
+	ssc.stop()
+
